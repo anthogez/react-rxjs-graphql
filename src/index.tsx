94db57168ccf6f9353b44d934e63028ee5d59ce6
anthogez <anthogez.gomes@gmail.com>
2019-12-14 16:20:14 +0100

    moving header to where it was meant.

--- src/index.tsx
+++ src/index.tsx
@@ -7,33 +7,17 @@ import { Route, Switch } from "react-router";
 
 import "antd/dist/antd.css";
 import "./index.css";
-import { HomePage } from "./pages";
+import { HomePage, ServicesPage } from "./pages";
 import { Toolbar } from "./components";
 
 const Main: React.FC<any> = () => {
   return (
     <ApolloProvider client={client}>
       <BrowserRouter>
+        <Toolbar />
         <Switch>
-          <Route
-            exact
-            path="/"
-            component={() => (
-              <div>
-                <Toolbar />
-                <HomePage />
-              </div>
-            )}
-          />
-          <Route
-            path="/services"
-            component={() => (
-              <div>
-                <Toolbar />
-                <div>Services</div>
-              </div>
-            )}
-          />
+          <Route exact path="/" component={() => <HomePage />} />
+          <Route path="/services" component={() => <ServicesPage />} />
         </Switch>
       </BrowserRouter>
     </ApolloProvider>
